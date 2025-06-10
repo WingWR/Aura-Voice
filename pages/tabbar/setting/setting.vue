98d7b1f31ffa6aa8acca0b05eb19896962302c52
Wing_WR <wingwr998@gmail.com>
2025-06-11 04:17:33 +0800

    20250611: 配置好框架

---
+++ pages/tabbar/setting/setting.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text>设置页面内容待添加</text>
+  </view>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #ffffff;
+}
+</style>
---
+++ pages/tabbar/voice/voice.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="container">
+    <view class="mic-button">
+      <button class="circle-button" type="primary">
+        
+      </button>
+    </view>
+  </view>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #ffffff;
+}
+
+.circle-button {
+  width: 100rpx;
+  height: 100rpx;
+  border-radius: 50%;
+  font-size: 50rpx;
+  background-color: #48d1cc;
+  color: white;
+  line-height: 100rpx;
+  text-align: center;
+}
+</style>
